7c4b63f0eb60866082a8886484b69122eebe25ee
Saratikyan Mher <saratikyanmher20@gmail.com>
2023-12-29 16:47:26 +0400

    separated interface and implementation

--- Vector/Vector.cpp
+++ Vector/Vector.cpp
@@ -1,72 +1,300 @@
-#include <iostream>
-#include "Vector.h"
+#ifndef VECTOR_TPP
+#define VECTOR_TPP
+
+#include "Vector.hpp"
 
 template <typename T>
-void infoAboutVec(const Vector<T>& v){
-    std::cout<<"\nsize: "<<v.getSize()
-            <<"\ncapacity: "<<v.getCapacity()
-            <<"\nis empty: "<<(v.empty() ? "true" : "false")
-            <<"\nElements: ";
-            for(size_t i{0};i < v.getSize();++i){
-                std::cout<<v[i]<<' ';
-            }
+vector<T>::vector()
+: m_size{0}
+, m_capacity{0}
+, m_vec{nullptr} 
+{}
+
+template <typename T>
+vector<T>::vector(size_type count)
+: m_size{count}
+, m_capacity{count}
+, m_vec{new T[count]} 
+{}
+
+template <typename T>
+vector<T>::vector(size_type count, const T& value)
+: m_size{count}
+, m_capacity{count}
+, m_vec{new T[count]} 
+{
+    for(int i{0};i < m_size; ++i){
+        m_vec[i] = value;
+    }
 }
-int main(){
-    Vector<int> v1;
-    std::cout<<"\nv1:";
-    infoAboutVec(v1);
 
-    Vector<int> v2(10);
-    std::cout<<"\nv2:";
-    infoAboutVec(v2);
-
-    Vector<int> v3(10,42);
-    std::cout<<"\nv3:";
-    infoAboutVec(v3);
-
-    Vector<int> v4{v3};
-    std::cout<<"\nv4:";
-    infoAboutVec(v4);
-
-    Vector<int> v5;
-    v5 = v4;
-    std::cout<<"\nv5:";
-    infoAboutVec(v5);
-
-    v5.push_back(12);
-    std::cout<<"\nv5:";
-    infoAboutVec(v5);
-
-    std::cout<<"\nTseting move constructor:";
-    std::cout<<"\nVector v5:";
-    infoAboutVec(v5);
-
-    Vector<int> v6(std::move(v5));
-
-    std::cout<<"\nVector v6:";
-    infoAboutVec(v6);
-
-    std::cout<<"\nMoved-from v5:";
-    infoAboutVec(v5);
-
-    std::cout<<"\n\nTesting move assignment:\n";
-    Vector<int> v7;
-    std::cout<<"Newly created v7:";
-    infoAboutVec(v7);
-
-    v7 = std::move(v6);
-
-    std::cout<<"\nVector v7 after assigning to it v6:";
-    infoAboutVec(v7);
-
-    std::cout<<"\nNew state of Vector v6:";
-    infoAboutVec(v6);
-
-    v7.insert(4,33);
-    std::cout<<"\nVector v7 after inserting 33 at index 4:";
-    infoAboutVec(v7);
-
-    v7.remove(4);
-    std::cout<<"\nVector v7 after removing element in from the index 4:";
-    infoAboutVec(v7);
+template <typename T>
+vector<T>::vector(std::initializer_list<T> init)
+{
+    reserve(init.size());
+    for(const auto& elem : init){
+        push_back(elem);
+    }
 }
+
+template <typename T>
+vector<T>::vector(const vector<T>& other)
+: m_size{other.m_size}
+, m_capacity{other.m_size}
+, m_vec{new T[other.m_size]}
+{
+    for(int i{0};i < m_size; ++i){
+        m_vec[i] = other.m_vec[i];
+    }
+}
+
+template <typename T>
+vector<T>::vector(vector<T>&& other) noexcept
+: m_size{other.m_size}
+, m_capacity{other.m_capacity}
+, m_vec{other.m_vec} 
+{
+    other.m_capacity = 0;
+    other.m_size = 0;
+    other.m_vec = nullptr;
+}
+
+template <typename T>
+vector<T>::~vector()
+{
+    delete[] m_vec;
+}
+
+template <typename T>
+vector<T>& vector<T>::operator=(const vector<T>& other)
+{
+    vector<T> tmp(other);
+    swap(tmp);
+    return *this;
+}
+
+template <typename T>
+vector<T>& vector<T>::operator=(vector<T>&& other) noexcept
+{
+    m_size = std::exchange(other.m_size, 0);
+    m_capacity = std::exchange(other.m_capacity, 0);
+    m_vec = std::exchange(other.m_vec, nullptr);
+    return *this;
+}
+
+template <typename T>
+vector<T>& vector<T>::operator=(std::initializer_list<T> init)
+{
+    vector<T> tmp(init);
+    swap(tmp);
+    return *this;
+}
+
+template <typename T>
+void vector<T>::assign(size_type count, const T& value)
+{
+    if(count > m_capacity){
+        realloc(count);
+    }
+    m_size = 0;
+    push_back(value);
+
+}
+
+template <typename T>
+void vector<T>::assign(std::initializer_list<T> init)
+{
+    if(init.size() > m_capacity){
+        realloc(init.size());
+    }
+
+    m_size = 0;
+    for(const auto& elem : init){
+        push_back(elem);
+    }
+}
+
+template <typename T>
+typename vector<T>::const_reference vector<T>::at(size_type pos) const
+{
+    if(pos < 0 || pos >= m_size){
+        throw std::out_of_range("Attempt to access a subscript out of range");
+    }
+
+    return m_vec[pos];
+}
+
+template <typename T>
+typename vector<T>::reference vector<T>::at(size_type pos)
+{
+    return const_cast<T&>(std::as_const(*this).at(pos));
+}
+
+template <typename T>
+typename vector<T>::const_reference vector<T>::operator[](size_type pos) const
+{
+    return m_vec[pos];
+}
+
+template <typename T>
+typename vector<T>::reference vector<T>::operator[](size_type pos)
+{
+    return const_cast<T&>(std::as_const(*this)[pos]);
+}
+
+template <typename T>
+typename vector<T>::const_reference vector<T>::front() const
+{
+    return m_vec[0];
+}
+
+template <typename T>
+typename vector<T>::reference vector<T>::front()
+{
+    return const_cast<T&>(std::as_const(*this).front());
+}
+
+template <typename T>
+typename vector<T>::const_reference vector<T>::back() const
+{
+    return m_vec[m_size - 1];
+}
+
+template <typename T>
+typename vector<T>::reference vector<T>::back()
+{
+    return const_cast<T&>(std::as_const(*this).back());
+}
+
+template <typename T>
+const T* vector<T>::data() const
+{
+    return m_vec;
+}
+
+template <typename T>
+T* vector<T>::data()
+{
+    return const_cast<T*>(std::as_const(*this).data());
+}
+
+
+template <typename T>
+bool vector<T>::empty() const
+{
+    return (m_size == 0);
+}
+
+template <typename T>
+typename vector<T>::size_type vector<T>::size() const
+{
+    return m_size;
+}
+
+template <typename T>
+typename vector<T>::size_type vector<T>::capacity() const
+{
+    return m_capacity;
+}
+
+template <typename T>
+void vector<T>::reserve(size_type new_cap)
+{
+    realloc(new_cap);
+}
+
+template <typename T>
+void vector<T>::shrink_to_fit()
+{
+    realloc(m_size);
+}
+
+template <typename T>
+void vector<T>::push_back(const T& value)
+{
+    if(m_size == m_capacity){
+        realloc(2 * m_capacity);
+    }
+
+    m_vec[m_size] = value;
+    ++m_size;
+}
+
+template <typename T>
+void vector<T>::pop_back()
+{
+    if(empty()){
+        throw std::out_of_range("Attempt to call pop_back on an empty vector");
+    }
+
+    --m_size;
+}
+
+template <typename T>
+void vector<T>::insert(size_type pos, const T& value)
+{
+
+    if(pos < 0 || pos >= m_size){
+        throw std::out_of_range("Subscript out of range");
+    }
+    if(m_size == m_capacity){
+        realloc(2 * m_capacity);
+    }
+
+    for(int i{m_size};i > pos; --i){
+        m_vec[i] = m_vec[i - 1];
+    }
+
+    m_vec[pos] = value;
+    ++m_size;
+}
+
+template <typename T>
+void vector<T>::clear()
+{
+    m_size = 0;
+    m_capacity = 0;
+    delete[] m_vec;
+    
+}
+
+template <typename T>
+void vector<T>::swap(vector<T>& other)
+{
+    std::swap(m_size, other.m_size);
+    std::swap(m_capacity, other.m_capacity);
+    std::swap(m_vec, other.m_vec);
+}
+
+template <typename U>
+bool operator==(const vector<U>& lhs, const vector<U>& rhs)
+{
+    if(lhs.size() != rhs.size()){
+        return false;
+    }
+
+    for(int i{0};i < rhs.size(); ++i){
+        if(lhs[i] != rhs[i]){
+            return false;
+        }
+    }
+
+    return true;
+}
+
+template <typename T>
+void vector<T>::realloc(size_type new_cap){
+    if(new_cap == 0){
+        new_cap = 1;
+    }
+
+    T* tmp = new T[new_cap];
+    for(int i{0};i < m_size; ++i){
+        tmp[i] = m_vec[i];
+    }
+
+    delete[] m_vec;
+    m_vec = tmp;
+}
+
+#endif
---
+++ Vector/Vector.hpp
@@ -0,0 +1,95 @@
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+
+#include <cstddef>
+#include <utility>
+#include <initializer_list>
+#include <stdexcept>
+
+template <typename T>
+class vector{
+
+public:
+    using value_type = T;
+    using reference = value_type&;
+    using const_reference = const value_type&;
+    using size_type = std::size_t;
+    using difference_type = std::ptrdiff_t;
+
+public:
+    vector();
+    explicit vector(size_type count);
+    vector(size_type count, const T& value);
+    vector(const vector<T>& other);
+    vector(vector<T>&& other) noexcept;
+    vector(std::initializer_list<T> init);
+    ~vector();
+
+public:
+    vector& operator=(const vector& other);
+    vector& operator=(vector&& other) noexcept;
+    vector& operator=(std::initializer_list<T> init);
+
+    void assign(size_type count, const T& value);
+    void assign(std::initializer_list<T> init);
+
+//element access
+public:
+    //access specified element with bound checking
+    const_reference at(size_type pos) const;
+    reference at(size_type pos);
+    
+    
+    //access specified element
+    const_reference operator[](size_type pos) const;
+    reference operator[](size_type pos);
+    
+
+    //acces the first element
+    const_reference front() const;
+    reference front();
+    
+
+    //access the last element
+    const_reference back() const;
+    reference back();
+    
+
+    //access the underlying contiguous stotage
+    const T* data() const;
+    T* data();
+    
+
+//Capacity
+public:
+    bool empty() const;
+    size_type size() const;
+    size_type capacity() const;
+    void reserve(size_type new_cap);
+    void shrink_to_fit();
+
+//Modifiers
+public:
+    void push_back(const T& value);
+    void pop_back();
+    void insert(size_type pos, const T& value);
+    void clear();
+    void swap(vector& other);
+
+//non member functions
+public:
+    template <typename U>
+    friend bool operator==(const vector<U>& lhs, const vector<U>& rhs);
+
+
+//utility functions
+private:
+    void realloc(size_type new_cap);
+
+//data members
+private:
+    std::size_t m_size;
+    std::size_t m_capacity;
+    T* m_vec;
+};
+#endif
