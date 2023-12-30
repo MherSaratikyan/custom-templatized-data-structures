c4c02171b346cbabf0bc09bdd98ffc8935d5e7b9
Saratikyan Mher <saratikyanmher20@gmail.com>
2023-12-30 10:49:35 +0400

    added iterator

--- Vector/Vector.hpp
+++ Vector/Vector.hpp
@@ -10,6 +10,7 @@ template <typename T>
 class vector{
 
 public:
+
     using value_type = T;
     using reference = value_type&;
     using const_reference = const value_type&;
@@ -81,12 +82,130 @@ public:
     template <typename U>
     friend bool operator==(const vector<U>& lhs, const vector<U>& rhs);
 
+//iterators
+public:
+    //iterator
+    struct vector_iterator{
+        using value_type = typename vector<T>::value_type;
+        using difference_type = std::ptrdiff_t;
+        using iterator_category = std::random_access_iterator_tag;
+        using pointer = value_type*;
+        using reference = value_type&;
+        using iterator_type = typename vector<T>::vector_iterator;
+
+        vector_iterator(pointer ptr): m_ptr{ptr} {}
+
+        reference operator*()
+        {
+            return *(m_ptr);
+        }
+
+        pointer operator->(){
+            return m_ptr;
+        }
+
+        vector_iterator& operator++()
+        {
+            ++m_ptr;
+            return *this; 
+        }
+
+        vector_iterator operator++(int)
+        {
+            vector_iterator tmp(*this);
+            ++m_ptr;
+            return tmp;
+        }
+
+        vector_iterator& operator--()
+        {
+            --m_ptr;
+            return *this;
+        }
+
+        vector_iterator operator--(int)
+        {
+            vector_iterator tmp(*this);
+            --m_ptr;
+            return tmp;
+        }
+
+        vector_iterator operator+(difference_type n)
+        {
+            vector_iterator tmp{m_ptr + n};
+            return tmp;
+        }
+
+        vector_iterator operator-(difference_type n)
+        {
+            vector_iterator tmp{m_ptr - n};
+            return tmp;
+        }
+
+        difference_type operator-(const vector_iterator& other)
+        {
+            return (m_ptr - other.m_ptr);
+        }
+
+        vector_iterator& operator+=(difference_type n)
+        {
+            m_ptr += n;
+            return *this;
+        }
+
+        vector_iterator& operator-=(difference_type n)
+        {
+            m_ptr -= n;
+            return *this;
+        }
+
+        reference operator[](difference_type pos){
+            return m_ptr[pos];
+        }
+
+        friend bool operator==(const vector_iterator& it1, const vector_iterator& it2)
+        {
+            return (it1.m_ptr == it2.m_ptr);
+        }
+
+        friend bool operator<(const vector_iterator& it1, const vector_iterator& it2)
+        {
+            return (it1.m_ptr < it2.m_ptr);
+        }
+
+        friend bool operator>(const vector_iterator& it1, const vector_iterator& it2)
+        {
+            return (it1.m_ptr > it2.m_ptr);
+        }
+
+        friend bool operator>=(const vector_iterator& it1, const vector_iterator& it2)
+        {
+            return (it1.m_ptr >= it2.m_ptr);
+        }
+
+        friend bool operator<=(const vector_iterator& it1, const vector_iterator& it2)
+        {
+            return (it1.m_ptr <= it2.m_ptr);
+        }
+
+        friend bool operator!=(const vector_iterator& it1, const vector_iterator& it2)
+        {
+            return (it1.m_ptr != it2.m_ptr);
+        }
+    
+    private:
+        pointer m_ptr{nullptr};
+    };
+
+//iterator functions
+public:
+    vector_iterator begin();
+    vector_iterator end();
 
 //utility functions
 private:
     void copy(const vector& other);
     void realloc(size_type new_cap);
-
 //data members
 private:
     std::size_t m_size;
